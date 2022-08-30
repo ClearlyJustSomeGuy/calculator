aef854ed28d86b202d34e449781e99259e5f9e9b
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-30 14:23:46 -0600

    Create buttons and act on them

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-function add(numOne, numTwo) {
-    return numOne + numTwo;
-}
+function add(numOne, numTwo) {return numOne + numTwo;}
 
-function subtract(numOne, numTwo) {
-    return numOne - numTwo;
-}
+function subtract(numOne, numTwo) {return numOne - numTwo;}
 
-function multiply(numOne, numTwo) {
-    return numOne * numTwo;
-}
+function multiply(numOne, numTwo) {return numOne * numTwo;}
 
 function divide(numOne, numTwo) {
-    return numOne / numTwo;
-}
+    if (numTwo === 0) {
+        const display = document.querySelector("#display");
+        display.textContent = 'Div by zero err';
+        return;
+    }
+    return numOne / numTwo;}
 
 function operate(numOne, numTwo, operator) {
     switch (operator) {
@@ -26,3 +24,34 @@ function operate(numOne, numTwo, operator) {
             return divide(numOne, numTwo);
     }
 }
+
+let displayNum = [];
+let opArr = ['plus', 'minus', 'multiply', 'divide'];
+
+// Key presses by storing each number into an array using push
+function pushNumDisplay(digit) {
+    displayNum.push(digit);
+    const display = document.querySelector("#display");
+    display.textContent = displayNum.join('');
+
+}
+
+
+// Grab clicks and parse their values
+const buttons = document.querySelectorAll(".container button");
+buttons.forEach((button => {
+    button.addEventListener('click', () => {
+        if (parseInt(button.id) >= 0 && parseInt(button.id) <= 9) {
+            pushNumDisplay(button.id);
+        } else if (opArr.indexOf(button.id) > -1) {
+            console.log(button.id);
+        } else {
+            console.log(button.id);
+        }
+    })
+}));
+
+// Display array into display div
+const display = document.querySelector("#display");
+// On operator click store number into valueOne and then start adding 
+//      to new variable valueTwo
